efe30de654b07c5d60f92b8e87a6f3c1adffaea7
Dave Castaldo <djcastaldo@gmail.com>
2025-07-17 02:07:05 -0400

    keycode and layer updates

--- keyboards/lemokey/p1_pro/ansi_encoder/keymaps/djcastaldo/keymap.c
+++ keyboards/lemokey/p1_pro/ansi_encoder/keymaps/djcastaldo/keymap.c
@@ -212,7 +212,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //  : |_________||________||_________||______________________________________________||______||______|______|  |______||______||______| :
 //  `-----------------------------------------------------------------------------------------------------------------------------------`
     [MAC_BASE_LAYR] = LAYOUT_ansi_82(
-        DUAL_ESC,  KC_F1, KC_F2, KC_F3,  KC_F4,   KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, DUAL_F12,  KC_APP, ENC_DUALPUSH,
+        DUAL_ESC,  KC_F1, KC_F2, KC_F3,  KC_F4,   KC_F5, KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, DUAL_F12, KC_LPAD, ENC_DUALPUSH,
         KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0, KC_MINS, KC_EQL,   BSPCFAST,      KC_DEL,
         LT(TMUX_LAYR,KC_TAB),  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P, KC_LBRC, KC_RBRC, KC_BSLS,   KC_PGUP,
         TD(CAPS_LAYR),  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,    KC_K,  KC_L,  KC_SCLN, KC_QUOT,      KC_ENT,   KC_PGDN,
@@ -1929,8 +1929,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         break;
     case KC_CALC:
         if (is_mac_base() && record->event.pressed) {
-            // send command + control + * then delay and then h
-            send_string(SS_LCTL(SS_LCMD(SS_TAP(X_PAST))) SS_DELAY(25) "h");
+            // send command + control + * then delay and then h (setup to open calculator pro)
+            send_string(SS_LCTL(SS_LCMD(SS_TAP(X_PAST))) SS_DELAY(35) "h");
             return false;
         }
         break;
