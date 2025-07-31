c91422fd2ce4759b8bf470bae010868eb18bcefa
Dave Castaldo <djcastaldo@gmail.com>
2025-07-31 10:10:49 -0400

    update userspace rules.mk

--- users/djcastaldo/rules.mk
+++ users/djcastaldo/rules.mk
@@ -1,7 +1,9 @@
-$(info >>> LOADING $(USER_PATH) rules.mk <<<)
-SRC += users/djcastaldo/features/layer_lock.c
-SRC += users/djcastaldo/process_record_userspace.c
+$(info >>> LOADING $(USER_PATH)/rules.mk <<<)
+LLOCK_PATH := $(USER_PATH)/features/layer_lock.c
+PROCESS_RECORD_PATH := $(USER_PATH)/process_record_userspace.c
 SECRETS_PATH := $(USER_PATH)/secrets.c
+SRC += $(LLOCK_PATH)
+SRC += $(PROCESS_RECORD_PATH)
 ifneq ("$(wildcard $(SECRETS_PATH))","")
   SRC += $(SECRETS_PATH)
 endif
