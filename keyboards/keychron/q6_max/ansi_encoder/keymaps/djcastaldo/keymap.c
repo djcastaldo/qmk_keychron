a98efeabfa10c50e0ae90faf26b9d3e5dbf83bd0
Dave Castaldo <djcastaldo@gmail.com>
2025-08-16 22:25:36 -0400

    add DUAL_ESC to q6 keymap

--- keyboards/keychron/q6_max/ansi_encoder/keymaps/djcastaldo/keymap.c
+++ keyboards/keychron/q6_max/ansi_encoder/keymaps/djcastaldo/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //:|_____||_____||_____||_____________________________________||_____||_____||_____||______| |____||____||____| |__________||____||____|:
 //`-------------------------------------------------------------------------------------------------------------------------------------`
     [MAC_BASE] = LAYOUT_109_ansi(
-        KC_ESC,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,ENC_MUTEPLAY,
+        DUAL_ESC,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_F11,KC_F12,ENC_MUTEPLAY,
                                                                              DUAL_SNAP,KC_SIRI,AP_GLOB,DUAL_F13,DUAL_F14,KC_CALC,LOCKSCR,
         KC_GRV,KC_1,KC_2,KC_3,KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, BSPCFAST,
                                                                                  KC_INS,KC_HOME,KC_PGUP,KC_NUM,KC_PSLS,KC_PAST,DUAL_PMNS,
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //:|_____||_____||_____||_____________________________________||_____||_____||_____||______| |____||____||____| |__________||____||____|:
 //`-------------------------------------------------------------------------------------------------------------------------------------`
     [WIN_BASE] = LAYOUT_109_ansi(
-        KC_ESC,KC_F1,KC_F2,KC_F3,KC_F4, KC_F5,KC_F6,KC_F7,KC_F8, KC_F9,KC_F10,KC_F11,KC_F12,ENC_MUTEPLAY,
+        DUAL_ESC,KC_F1,KC_F2,KC_F3,KC_F4, KC_F5,KC_F6,KC_F7,KC_F8, KC_F9,KC_F10,KC_F11,KC_F12,ENC_MUTEPLAY,
                                                                             KC_PSCR,KC_CTANA,RGB_MOD,  DUAL_F13,DUAL_F14,KC_CALC,LOCKSCR,
         KC_GRV,KC_1,KC_2,KC_3,KC_4,KC_5,KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINS,KC_EQL,BSPCFAST,KC_INS,KC_HOME,KC_PGUP,
                                                                                                         KC_NUM,KC_PSLS,KC_PAST,DUAL_PMNS,
