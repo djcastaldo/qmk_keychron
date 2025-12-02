d38eed98cf8f0e479443c97ea4f975c2111f39bc
Dave Castaldo <djcastaldo@gmail.com>
2025-12-01 23:06:46 -0500

    tmux knob updates; wake from sleep improvements

--- keyboards/keychron/q65_max/ansi_encoder/config.h
+++ keyboards/keychron/q65_max/ansi_encoder/config.h
@@ -43,7 +43,7 @@
 /* Allow shutdown of led driver to save power */
 #    define RGB_MATRIX_DRIVER_SHUTDOWN_ENABLE
 /* Turn off backlight on low brightness to save power */
-#    define RGB_MATRIX_BRIGHTNESS_TURN_OFF_VAL 32
+#    define RGB_MATRIX_BRIGHTNESS_TURN_OFF_VAL 0
 
 /* Indications */
 #    define CAPS_LOCK_INDEX 32
@@ -53,4 +53,101 @@
 #    define RGB_MATRIX_KEYPRESSES
 #    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
+/* rgb settings */
+#    define RGB_MATRIX_DEFAULT_ON true
+#    define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+#    define RGB_MATRIX_DEFAULT_HUE 170
+#    define RGB_MATRIX_DEFAULT_SAT 235
+#    define RGB_MATRIX_DEFAULT_VAL 255
+#    define RGB_MATRIX_DEFAULT_SPD 96
+
 #endif
+
+/* force enable NKRO */
+#    define FORCE_NKRO
+
+/* tap time for tap dancing */
+#    define TAPPING_TERM 250
+
+/* enable caps word when both shift keys are tapped */
+#    define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+/* do not allow dynamic macros to nest (can cause infinite loop) */
+#    define DYNAMIC_MACRO_NO_NESTING
+
+/* allow setting constant mode mouse cursor and wheel speeds */
+#    define MK_3_SPEED
+#    define MK_C_OFFSET_0 1
+#    define MK_C_INTERVAL_0 6
+#    define MK_C_OFFSET_1 4
+#    define MK_C_INTERVAL_1 8
+
+/* leader key settings */
+#    define LEADER_TIMEOUT 250
+#    define LEADER_PER_KEY_TIMING
+#    define LEADER_NO_TIMEOUT
+
+/* for tmux send_string delays */
+#    define TMUX_DELAY 25
+#    define CONFIG_VS_LAYR_SEND_STRING_DELAY 35
+
+/* monitored base layers for macos */
+#    define CONFIG_MACOS_BASE_LAYERS { MAC_BASE }
+#    define CONFIG_MACOS_BASE_LAYERS_COUNT 1
+#    define CONFIG_DEFAULT_MAC_LAYR MAC_BASE
+#    define CONFIG_DEFAULT_WIN_LAYR WIN_BASE
+
+/* monitored base layers for macos */
+#    define CONFIG_ALL_BASE_LAYERS { MAC_BASE, WIN_BASE }
+#    define CONFIG_ALL_BASE_LAYERS_COUNT 2
+/* key indexes that changed to shifted version when caps is used */
+#    define CONFIG_CAPSLOCK_SHIFTED ((i > 16 && i < 27) || (i > 32 && i < 42) || (i > 47 && i < 55))
+#    define CONFIG_CAPSWORD_EXTRA (i == 11)
+
+/* any nonstandard condition for keys that should not fade when pressed */
+//#    define CONFIG_CUSTOM_DO_NOT_FADE (key.index < 19 || key.index == 72)
+
+/* keyfade settings */
+#    define CONFIG_KEYFADE_START_DELAY 16
+#    define CONFIG_KEYFADE_START_VAL 250
+#    define CONFIG_KEYFADE_CALLBACK_INTERVAL 15
+
+/* rgb_matrix_indicators_advanced_user settings */
+#    define CONFIG_HAS_LLOCK_KEY
+#    define CONFIG_HAS_KLOCK_KEY
+#    define CONFIG_HAS_KCLK_BATTERY
+#    define CONFIG_FN_LAYR_COLOR RGB_GREEN
+#    define CONFIG_MREC_KEY_COLOR RGB_CORAL
+#    define CONFIG_SHIFT_LAYR_COLOR RGB_ORANGE
+#    define CONFIG_KCTL_LAYR_COLOR RGB_RED
+#    define CONFIG_TMUX_LAYR_COLOR RGB_CYAN
+#    define CONFIG_VS_LAYR_COLOR RGB_PURPLE
+#    define CONFIG_SYM_LAYR_COLOR RGB_SPRINGGREEN
+#    define CONFIG_WIDE_LAYR_COLOR RGB_TURQUOISE
+#    define CONFIG_CIRC_LAYR_COLOR RGB_CORAL
+#    define CONFIG_EMO_LAYR_COLOR RGB_YELLOW
+#    define CONFIG_DEFUALT_LAYR_COLOR 0x77,0x77,0x77
+#    define CONFIG_EXTRA_BASE_LAYR_COLORS \
+     { \
+         { I_CAPS, RGB_BLUE }, \
+         { I_FN, RGB_BLUE }, \
+         { I_TAB, 0x77,0x77,0x77 }, \
+         { I_BSLS, 0x77,0x77,0x77 }, \
+     }
+#    define CONFIG_EXTRA_BASE_LAYR_COLORS_COUNT 4
+#    define CONFIG_ACCENT_KEY_COLOR RGB_YELLOW
+#    define CONFIG_CAPS_WORD_SHIFT_COLOR 0x77,0x77,0x77
+#    define CONFIG_HROWLIGHT_COLOR RGB_GREEN
+#    define CONFIG_FJLIGHT_COLOR RGB_WHITE
+#    define CONFIG_LEADER_COLORA RGB_RED
+#    define CONFIG_LEADER_COLORB RGB_WHITE
+#    define CONFIG_KEYLIGHT_STD_COLOR RGB_WHITE
+#    define CONFIG_KEYS_FADE_TO_BLUE
+#    define CONFIG_EEPROM_RESET_DEFAULT_LAYER MAC_BASE
+#    define CONFIG_KCLK_24G_HOST_IDX 21
+#    define CONFIG_KCLK_BT_KEY_START_IDX 16
+#    define CONFIG_HAS_BASE_LAYER_TOGGLE
+#    define CONFIG_SWITCH_PIN B12
+#    define CONFIG_LOCK_ANIMATION_TIMEOUT 300000
+#    define CONFIG_LOCK_RESTORE_ANIMATION_FROM_SUSPEND_MS 200
+#    define CONFIG_LOCK_ANIMATION_COLOR_HSV HSV_RED
