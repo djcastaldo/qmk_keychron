23aca3bf99070f31354ebf4d4f2c4b3b7072ea6d
Dave Castaldo <djcastaldo@gmail.com>
2025-12-28 01:51:14 -0500

    updates for userspace dip switch functionality on q65_max

--- keyboards/keychron/q65_max/q65_max.c
+++ keyboards/keychron/q65_max/q65_max.c
@@ -26,15 +26,17 @@
 #    include "keychron_wireless_common.h"
 #    include "battery.h"
 #endif
+#include "layers.h"
+#include "process_record_userspace.h"
 
 #ifdef DIP_SWITCH_ENABLE
 bool dip_switch_update_kb(uint8_t index, bool active) {
     if (index == 0) {
-        default_layer_set(1UL << (active ? 1 : 0));
+        default_layer_set(1UL << (active ? WIN_BASE : MAC_BASE)); // djc: modified
     }
     dip_switch_update_user(index, active);
 
-    return true;
+    return dip_switch_update_userspace(index, active); // djc
 }
 #endif
 
